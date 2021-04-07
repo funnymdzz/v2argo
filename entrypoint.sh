684825fdbd5b106b1af6845432813a9b7f2be9fc
funnymdzz <funnymdzz@gmail.com>
2021-04-07 16:43:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 cd /v2raybin
-sed -i "s/PORT/$PORT/g" config.json
 ./v2ray &
 cd /argobin
 echo $JSON > argo.json
 echo $CERT > cert.pem
-sed -i "s/PORT/$PORT/g" config.yml
 sed -i "s/ID/$ID/g" config.yml
 ./argo run --cred-file cert.pem --config config.yml
