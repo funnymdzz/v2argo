109977c87c880bfce5d04b5187d079363324fbc5
funnymdzz <funnymdzz@gmail.com>
2021-04-07 17:26:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo $JSON > argo.json
 echo $CERT > cert.pem
 sed -i "s/PORT/$PORT/g" config.yml
 sed -i "s/ID/$ID/g" config.yml
-./argo run --cred-file cert.pem --config config.yml
+./argo --cred-file cert.pem  tunnel -config new.yml run cm
