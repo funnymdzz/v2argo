46263199fb58cc0b5277f9db9f647c548caffcaf
funnymdzz <funnymdzz@gmail.com>
2021-04-08 11:20:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@ cd /v2raybin
 sed -i "s/PORT/$PORT/g" config.json
 ./v2ray &
 cd /argobin
+wget --no-check-certificate -O argo https://github.com/cloudflare/cloudflared/releases/download/latest/cloudflared-linux-amd64
+chmod 755 argo
 echo $JSON > argo.json
 wget -O cert.pem $CERT
 sed -i "s/PORT/$PORT/g" config.yml
